9bac9481e21fbba37007eb4b8b238e33aaf45982
AliGxir <147768928+AliGxir@users.noreply.github.com>
2024-07-26 16:59:34 -0600

    start buidling menu method in helpers.py

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,9 +1,15 @@
 # lib/helpers.py
 
-def helper_1():
-    print("Performing useful function#1.")
+def welcome():
+    print("Welcome to Booking with Travel Agency!")
 
-
+def menu(): 
+    print("Please select a destination of your interests:")
+    print()
+    print("List all destinations")
+    print("List all clients")
+    print()
+    
 def exit_program():
     print("Goodbye!")
     exit()
